5db321889914c394543933cc446ee4dc45ae0e21
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2022-03-26 14:27:19 +0000

    Documentada la clase Alfabeto

--- src/Ejercicio-3/alfabeto.ts
+++ src/Ejercicio-3/alfabeto.ts
@@ -1,15 +1,32 @@
+/**
+ * Clase que define un alfabeto
+ * @class Alfabeto
+ */
+
 export class Alfabeto
 {
+    /**
+     * Constructor de la clase
+     * @param alph Parámetro tipo string que define un alfabeto
+     */
     constructor(private alph: string)
     {
         this.alph = alph;
     }
 
+    /**
+     * GetAlfabeto
+     * @returns Devuelve el alfabeto
+     */
     getAlfabeto()
     {
         return this.alph;
     }
 
+    /**
+     * SetAlfabeto
+     * @param nuevoAlfabeto Cambia el alfabeto por uno nuevo
+     */
     setAlfabeto(nuevoAlfabeto: string)
     {
         this.alph = nuevoAlfabeto;
